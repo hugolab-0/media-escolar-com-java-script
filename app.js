207a188547f804826468f05fb8453011ecfb3e31
HUGO DE OLIVEIRA ALVES VIEIRA <25203638@CFP127ALUNO.LOCAL>
2026-02-04 08:48:45 -0300

    media escolar

---
+++ app.js
@@ -0,0 +1,130 @@
+/*************************************************************
+ * Objetivo: Calcular médias escolares
+ * Data: 29/01/2026
+ * Autor: Anderson
+ * Versão: 1.0
+ ************************************************************/
+
+
+/*
+    Existem 3 formas de criação de variáveis
+
+    var -> Permite a criação de um espaço na memoriado
+            do tipo variável. Foi utilizado muito em projetos antigos
+            Recomendação: Caso você queira utilizar,
+            recomenda-se na criação de variáveis globais
+            (inicio do código)
+            
+    let -> Permite a criação de um espaço na memória 
+            do tipo variável. A utilização deste padrão é
+            para a criação dentro de blocos de programação { }.
+            Essa variável nasce e morre dentro deste bloco.
+            Não é recomendado a sua utilização em escopo global.
+
+    const -> Permite a criação de um espaço na memória
+            onde não sofrerá alteração durante o código. A const
+            pode ser utilizada dentro e fora de bloco { }.
+            Dica: Caso você queira diferenciar uma const, um var ou
+            um let.
+            A const você pode criar com letras MAIUSCULAS.
+
+*/
+
+
+//Import da biblioteca
+const readline = require('readline')
+
+//Cria o objeto para entrada de dados
+const entradaDeDados = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+//Entrada do nome
+entradaDeDados.question('Digite o nome do aluno:', function(nome){
+    //Recebe o nome do aluno que foi digitado
+    let nomeAluno = nome
+
+    //Entrada da nota1
+    entradaDeDados.question('Digite a nota 1:', function(valor1){
+        let nota1 = valor1
+
+        //Entrada da nota2
+        entradaDeDados.question('Digite a nota 2:', function(valor2){
+            let nota2 = valor2
+
+            //Entrada da nota3
+            entradaDeDados.question('Digite a nota 3:', function(valor3){
+                let nota3 = valor3
+
+                //Entrada da nota4
+                entradaDeDados.question('Digite a nota 4:', function(valor4){
+                    let nota4 = valor4
+
+                    /*
+                        Operadores de Comparação
+                    
+                        ==  -> Permite comparar a igualdade de dois constantes
+                        <   -> Permite comparar valores menores
+                        >   -> Permite comparar valores maiores
+                        >=  -> Permite comparar valores maiores ou iguais
+                        <=  -> Permite comparar valores menores ou iguais
+                        !=  -> Permite comparar a diferença entre conteúdos
+                        === -> Permite comparar o a idualdade de conteúdos 
+                                e a tipagem de dados
+                        !== -> Permite comparar a diferença de conteúdos e a
+                                igualdade de tipos de dados
+                        ==! -> Permite comparar a igualdade de conteúdos e a
+                                diferença de tipos de dados
+                        !=! -> Permite comparar a diferença de conteúdos e a
+                                diferença de tipos de dados
+
+                        Operadores Lógicos
+
+                            E   -> AND  -> &&
+                            OU  -> OR   -> ||
+                            NÃO -> NOT  -> !
+
+                    */
+                    //Validação de entrada vazia
+                    if (nomeAluno == '' || nota1 == '' || nota2 == '' || nota3 == '' || nota4 == ''){
+                        console.log('ERRO: é obrigatório o preenchimento de todos os dados !!!')
+                        ''
+                    //validaçâo de numeros de 0 e 100    
+                    }else if(nota1 < 0 || nota1 > 100 || nota2 < 0 || nota2 > 100 || nota3 < 0 || nota3 > 100 || nota4 < 0 || nota4 > 100){
+                        console.log('ERRO: as notas devem estar entre 0 à 100')
+
+                    //validacao para entrada de letras nas notas
+                    //isNaN() --> permite validar o conteúdo da 
+                       //variavel tem algum caracter ao invés de número    
+                    }else if(isNaN(nota1) || isNaN(nota2) || isNaN(nota3) || isNaN(nota4) ) {
+                        console.log('ERRO: as notas devem ser preenchidas somente com numeros')
+                    }
+                    else{
+                        /*
+                        
+                        conversões para tipos dados
+                        parseInt() -> permite converter um string para um numero inteiro
+                        parseFloat() -> permite converter um string para um numero inteiro
+                        Number() -> permite converter um string para um numero inteiro
+                        String() -> permite converter um um conteúdo par uma string
+                        Boolean() -> permite converter um conteúdo para BOOLEANO
+                        typeof() -> permite verificar o tipo de dado de uma variável
+
+                        */
+                        //Calcular a média
+                        let media = Number(nota1) + Number(nota2) + Number(nota3) + Number(nota4) / 4
+
+                        console.log('Nome do aluno: ' + nomeAluno)
+                        console.log('A média do aluno é: ' + media)
+                    }
+
+
+
+
+
+                })//Fecha a nota 4
+            })//Fecha a nota 3
+        })//Fecha a nota 2
+    })//Fecha a nota 1
+})//Fecha o nome
